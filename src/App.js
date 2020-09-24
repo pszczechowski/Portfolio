3f8a7a15dbe641e20faca9070596f6fe9075d56b
Piotrek1991 <s16096@pjwstk.edu.pl>
2020-09-24 15:32:34 +0200

    create a navigation bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import Projects from './Projects';
+import Articles from './Articles';
+import About from './About';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={Projects} />
+      <Route exact ="/articles" component={Articles} /> 
+      <Route exact ="/about" component={About} />
+
+           <div className="navigation">
+        <img src={logo} className="logo" alt="Logo Image" />
+        <div className="navigation-sub">
+
+          <Link to="/" className="item">Projects</Link>
+          <Link to="/articles" className="item">Articles</Link>
+          <Link to="/about" className="item">About</Link>
+        </div>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Projects.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Thumbnail from "./Thumbnail"
+ 
+function Projects(props) {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <Thumbnail
+     link="/twitter"
+     image="http://twitter-image-url.jpg"
+     title="Twitter Newsfeed"
+     category="Mobile App"
+      />
+    </div>
+  )
+}
+ 
+export default Projects;
