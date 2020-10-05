3463c3874b565ad06397cac4a74a7ecd5a07a53f
Piotrek1991 <s16096@pjwstk.edu.pl>
2020-10-05 18:41:56 +0200

    giving a look to navbar

--- src/App.js
+++ src/App.js
@@ -2,26 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
-import Projects from './Projects';
-import Articles from './Articles';
-import About from './About';
+import Projects from './Navbar/Projects';
+import About from './Navbar/About';
+import Contact from './Navbar/Contact';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 
 function App() {
   return (
     <BrowserRouter>
+    <div className="item">
+      <Link to="/" className="navbar">Projects</Link>
+      <Link to="/about" className="navbar">About</Link>
+      <Link to="/contact" className="navbar">Contact</Link>
+</div>
     <div className="App">
       <Route exact path="/" component={Projects} />
-      <Route exact ="/articles" component={Articles} /> 
-      <Route exact ="/about" component={About} />
+      <Route exact ="/about" component={Contact} /> 
+      <Route exact ="/contact" component={About} />
 
            <div className="navigation">
         <img src={logo} className="logo" alt="Logo Image" />
-        <div className="navigation-sub">
-
-          <Link to="/" className="item">Projects</Link>
-          <Link to="/articles" className="item">Articles</Link>
-          <Link to="/about" className="item">About</Link>
-        </div>
       </div>
     </div>
     </BrowserRouter>
