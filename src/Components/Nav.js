7a5550b4a1e6c8142327a1d4b2014ffe9941d80e
Piotrek1991 <s16096@pjwstk.edu.pl>
2020-12-06 15:25:13 +0100

    added on scroll function for navbar

---
+++ src/Components/Nav.js
@@ -0,0 +1,12 @@
+(function(){
+    var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("item").style.top = "0";
+  } else {
+    document.getElementById("item").style.top = "-240px";
+  }
+  prevScrollpos = currentScrollPos;
+} 
+})();
