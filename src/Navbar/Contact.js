5cd286a866c0a209c17cb6b244ec80a7d91ca4c2
Piotrek1991 <s16096@pjwstk.edu.pl>
2021-03-01 19:40:42 +0100

    add contact button

--- src/Navbar/Contact.js
+++ src/Navbar/Contact.js
@@ -2,6 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
   contact: {
@@ -50,20 +51,24 @@ const useStyles = makeStyles({
   contactForm: {
     display: 'flex',
     flexDirection: 'column',
-    width: '40%',
+    width: '50%',
+    '@media screen and (max-width: 1000px)': {
+      width: '70%',
+    },
   },
+  contactButton: {
+    display: 'flex',
+    justifyItems: 'center',
+    backgroundColor: 'black',
+    opacity: '0.7',
+    fontFamily: 'KalamRegular',
+  }
 })
 
 function Contact(props) {
   const classes = useStyles();
   return (
     <div id="contact" className={classes.contact}>
-      <div className={classes.socialLink}>
-        <a className={classes.icon} href="https://github.com/pszczechowski" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={["fab", "github"]} color="white" /></a>
-        <a className={classes.icon} href="https://www.linkedin.com/in/piotr-szczechowski-0649561b1/" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={["fab", "linkedin-in"]} color="white" /></a>
-      </div>
       <form className={classes.root} noValidate autoComplete="off">
         <div className={classes.contactForm}>
           <h1>Get in touch</h1>
@@ -73,6 +78,8 @@ function Contact(props) {
             type="email"
             autoComplete="current-email"
             variant="outlined"
+            validators={['required', 'isEmail']}
+            errorMessages={['this field is required', 'email is not valid']}
           />
           <TextField
             id="outlined-textarea"
@@ -84,6 +91,15 @@ function Contact(props) {
             margin="normal"
             variant="outlined"
           />
+          <Button className={classes.contactButton} href="#" variant="contained" color="primary">
+            Send
+          </Button>
+          <div className={classes.socialLink}>
+            <a className={classes.icon} href="https://github.com/pszczechowski" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={["fab", "github"]} color="white" /></a>
+            <a className={classes.icon} href="https://www.linkedin.com/in/piotr-szczechowski-0649561b1/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={["fab", "linkedin-in"]} color="white" /></a>
+          </div>
         </div></form>
       <span className={classes.footer}>Piotr Szczechowski © 2021. All rights reserved.</span>
     </div>
